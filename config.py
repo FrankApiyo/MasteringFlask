1a4f49c6c7eaea6317fe0a0af018128839f75ffd
FrankApiyo <franklineapiyo@gmail.com>
2019-05-22 16:52:08 +0300

    some more changes

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ class Config(object):
 
 class ProdConfig(Config):
     pass
-    
+
 class DevConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:Frankline@localhost/new_app_db"
+    SLQALCHEMY_ECHO = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
